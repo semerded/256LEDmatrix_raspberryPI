54a0d8719a2fb9328df97a949143e89b9ab4b182
Sem Van Broekhoven <sem.van.broekhoven@gmail.com>
2023-12-31 21:03:50 +0100

    testing

--- undo_redo.py
+++ undo_redo.py
@@ -24,19 +24,20 @@ class DrawingHistory:
     
     def checkForChanges(self, currentMatrix: list[list[int]], matrixRect):
         if self._checkForDifferenceInNewMatrix(currentMatrix) and not Interactions.isHoldingInRect(matrixRect, mouseButton.leftMouseButton.value): #TODO only add matrix when mouse is not pressed
-            
             self._checkIfHistoryNeedsOverwrite()
                 
             self.currentHistoryIndex += 1
             self.drawingHistory.append(deepcopy(currentMatrix))
+            self._checkIfHistoryNeedsOverwrite()
             
         self._checkForHistoryOverflow()
-        print(len(self.drawingHistory))
+        # print(len(self.drawingHistory), self.currentHistoryIndex)
+        print(self.drawingHistory[-1][0], currentMatrix[0])
     
     def _checkForDifferenceInNewMatrix(self, currentMatrix: list[list[int]]):
-        if self.drawingHistory[self.currentHistoryIndex] == currentMatrix:
-            return False
-        return True
+        if self.drawingHistory[-1] != currentMatrix:
+            return True
+        return False
     
     def _checkForHistoryOverflow(self):
         while len(self.drawingHistory) > 50:
