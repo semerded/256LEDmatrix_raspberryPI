029ff9b12f4b24e448374d6a1d5c188f24c094ef
Sem Van Broekhoven <sem.van.broekhoven@gmail.com>
2024-01-02 16:19:15 +0100

    fixed app crashing

--- loading_screen.py
+++ loading_screen.py
@@ -6,13 +6,13 @@ class LoadingScreen:
         self.text = text
         self.loadingEllipses = 0
         self.loadingWidget = Button((ScreenUnit.vw(50), ScreenUnit.vh(50)), Color.LIGHTGRAY, 5)
-        self.loadingWidget.text(text, Font.customFont(ScreenUnit.vw(4)), Color.GREEN, overFlow.show)
+        self.loadingWidget.text(text, Font.customFont(int(ScreenUnit.vw(4))), Color.GREEN, overFlow.show)
   
     def place(self):
             if self.APP.everyAmountOfTicks(20):
-                self.loadingWidget.text(self._loadingText(), Font.customFont(ScreenUnit.vw(4)), Color.GREEN, overFlow.show)
+                self.loadingWidget.text(self._loadingText(), Font.customFont(int(ScreenUnit.vw(4))), Color.GREEN, overFlow.show)
             self.loadingWidget.updateButtonSize(ScreenUnit.vw(50), ScreenUnit.vh(50))
-            self.loadingWidget.border(Color.GREEN, 5)
+            # self.loadingWidget.border(Color.GREEN, 5)
             self.loadingWidget.place(ScreenUnit.vw(25), ScreenUnit.vh(25))            
             pygame.display.update(self.loadingWidget.getRect)     
         
