7b20c88b71cffb308c7ba88fcabc6b096dc94188
Sem Van Broekhoven <sem.van.broekhoven@gmail.com>
2024-01-02 16:37:08 +0100

    added a working  loading screen when data is being send to matrix

--- loading_screen.py
+++ loading_screen.py
@@ -5,14 +5,15 @@ class LoadingScreen:
         self.APP = APP
         self.text = text
         self.loadingEllipses = 0
-        self.loadingWidget = Button((ScreenUnit.vw(50), ScreenUnit.vh(50)), Color.LIGHTGRAY, 5)
-        self.loadingWidget.text(text, Font.customFont(int(ScreenUnit.vw(4))), Color.GREEN, overFlow.show)
+        self.loadingWidget = Button((ScreenUnit.vw(50), ScreenUnit.vh(50)), Color.LIGHTGRAY, 20)
+        self.loadingWidget.text(text, Font.customFont(int(ScreenUnit.vw(8))), Color.GREEN, overFlow.show)
   
     def place(self):
-            if self.APP.everyAmountOfTicks(20):
-                self.loadingWidget.text(self._loadingText(), Font.customFont(int(ScreenUnit.vw(4))), Color.GREEN, overFlow.show)
+            self.APP.eventHandler(pygame.event.get())
+            if self.APP.everyAmountOfTicks(10):
+                self.loadingWidget.text(self._loadingText(), Font.customFont(int(ScreenUnit.vw(8))), Color.GREEN, overFlow.show)
             self.loadingWidget.updateButtonSize(ScreenUnit.vw(50), ScreenUnit.vh(50))
-            # self.loadingWidget.border(Color.GREEN, 5)
+            self.loadingWidget.border(10, Color.GREEN)
             self.loadingWidget.place(ScreenUnit.vw(25), ScreenUnit.vh(25))            
             pygame.display.update(self.loadingWidget.getRect)     
         
@@ -20,5 +21,7 @@ class LoadingScreen:
         self.loadingEllipses += 1
         if self.loadingEllipses > 3:
             self.loadingEllipses = 1
-        return self.text + ("." * self.loadingEllipses)
+        test = self.text + ("." * self.loadingEllipses)
+        # print(test)
+        return test
         
