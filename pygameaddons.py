7bf5c7a28afc866d23d397e5b430b3dd57b5b6d8
Sem Van Broekhoven <sem.van.broekhoven@gmail.com>
2024-01-05 22:15:33 +0100

    made scroll negative to work correctly

--- pygameaddons.py
+++ pygameaddons.py
@@ -306,18 +306,24 @@ class Scroll():
         self.maxScroll = maxScrollPixel
         self.scrollPosition = 0
         self.scrollSpeed = speed
+        self._makeMaxScrollNegative()
+        
+    def _makeMaxScrollNegative(self):
+        if self.maxScroll > 0:
+            self.maxScroll * -1
 
     def setMaxScroll(self, maxScrollPixel: int):
         self.maxScroll = maxScrollPixel
+        self._makeMaxScrollNegative()
         
     def setSpeed(self, speed: scrollSpeed):
         self.scrollSpeed = speed
         
     def scrollController(self) -> int:
         self.scrollPosition += scrollValue * self.scrollSpeed
-        if self.scrollPosition < 0:
+        if self.scrollPosition > 0:
             self.scrollPosition = 0
-        if self.scrollPosition > self.maxScroll:
+        if self.scrollPosition < self.maxScroll:
             self.scrollPosition = self.maxScroll        
         return self.scrollPosition
 
