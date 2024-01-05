fb1ba71f4e4446c8b17aeb0c1b1d3e41cb93d592
Sem Van Broekhoven <sem.van.broekhoven@gmail.com>
2024-01-05 22:33:08 +0100

    changed functionality of presets

--- presets.py
+++ presets.py
@@ -1,7 +1,7 @@
 from pygameaddons import *
 from matrix import Matrix
 from copy import deepcopy
-from math import ceil
+from functions import indexedLen
 import json
 
 class Presets:
@@ -10,6 +10,12 @@ class Presets:
         with open("presets.json") as presetsSaveFile:
             PRESETS = json.load(presetsSaveFile)
         self.createMatrixPresetList(PRESETS)
+        self.currentMatrixIndex = 0
+        self.buttonPrevious = Button((ScreenUnit.vh(10), ScreenUnit.vh(10)), Color.WHITE, 5)
+        self.buttonPrevious.text("<", Font.FONT50)
+        
+        self.buttonNext = Button((ScreenUnit.vh(10), ScreenUnit.vh(10)), Color.WHITE, 5)
+        self.buttonNext.text(">", Font.FONT100)
         
     
     def createMatrixPresetList(self, presets):
@@ -18,28 +24,28 @@ class Presets:
             tempMatrix = Matrix(len(matrix[0]), len(matrix))
             tempMatrix.setMatrix(matrix)
             self.matrixList.append(deepcopy(tempMatrix))
-        self.maxScrollValue = ScreenUnit.vw(50) * ceil(len(self.matrixList) / 2)
-        self.scroller = Scroll(self.maxScrollValue, scrollSpeed.medium.value)
-        self.scrollValue = 0
         
 
     def place(self):
         
-        self.scrollValue = self.scroller.scrollController()
+        if self.buttonPrevious.onMouseClick():
+            self.currentMatrixIndex -= 1
+            
+        if self.buttonNext.onMouseClick():
+            self.currentMatrixIndex += 1
         
-        if Interactions.isScrolled():
-            self.APP.requestUpdate
+        if self.currentMatrixIndex < 0:
+            self.currentMatrixIndex = indexedLen(self.matrixList)
+            
+        if self.currentMatrixIndex > indexedLen(self.matrixList):
+            self.currentMatrixIndex = 0
             
         self.APP.requestUpdate # TEMP
         
         if self.APP.firstFrame() or self.APP.updateAvalible:
             self.APP.maindisplay.fill(Color.BLACK)
-            
-            secondPosition = False
-            for index, matrix in enumerate(self.matrixList):
-                xPos = ScreenUnit.vw(3) if not secondPosition else ScreenUnit.vw(52)
-                yPos = ScreenUnit.vw(50) + ceil(index / 2)
-                matrix.drawMatrix((xPos, yPos), ScreenUnit.vw(45))
-                secondPosition = not secondPosition
+            self.buttonPrevious.place(ScreenUnit.vw(5), ScreenUnit.vh(45))
+            self.buttonNext.place(ScreenUnit.vw(5), ScreenUnit.vh(45))
+            self.matrixList[self.currentMatrixIndex].drawMatrix((ScreenUnit.vw(50) - (ScreenUnit.vh(95) / 2), ScreenUnit.vh(2.5)), ScreenUnit.vh(95))
             
             pygame.display.update()
