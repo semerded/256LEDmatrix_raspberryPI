afc03b712dd3716ac2aa93a42f606148b3cec19a
Sem Van Broekhoven <sem.van.broekhoven@gmail.com>
2024-03-07 20:38:08 +0100

    added base for 4th grade

--- newcode/core/mathBot.py
+++ newcode/core/mathBot.py
@@ -18,11 +18,11 @@ class MathBot:
     def getType(*validtypes: str):
         return random.choice(validtypes)
     
-    def getNumbers(maxRange, type):
+    def getNumbers(maxRange, type, minRange = 1):
         while True:
-            number1 = random.randint(1, maxRange)
-            number2 = random.randint(1, maxRange)
-            if MathBot.calculate(number1, number2, type) in range(1, maxRange + 1):
+            number1 = random.randint(minRange, maxRange)
+            number2 = random.randint(minRange, maxRange)
+            if MathBot.calculate(number1, number2, type) in range(minRange, maxRange + 1):
                 return number1, number2
             
     def maaltafel():
@@ -55,10 +55,27 @@ class MathBot:
     def getQuestion(classNumber):
         return classList[classNumber - 1]()
     
+    def vierdeJaar():
+        rand = random.randint(1, 2)
+        if rand == 1:
+            type = MathBot.getType("+", "-")
+            number1, number2 = MathBot.getNumbers(10000, type)
+        elif rand == 2:
+            type = MathBot.getType("+", "-")
+            number1, number2 = MathBot.getNumbers(10000, type, 1000)
+            number1 *= 10
+            number2 *= 10
+
+            
+            
+        solution = MathBot.calculate(number1, number2, type)
+        return str(number1), str(number2), type, solution
+    
 classList = [MathBot.eersteJaar, MathBot.tweedeJaar, MathBot.derdeJaar]
 
 if __name__ == "__main__":
     print(MathBot.eersteJaar())
     print(MathBot.tweedeJaar())
     print(MathBot.derdeJaar())
+    print(MathBot.vierdeJaar())
     
