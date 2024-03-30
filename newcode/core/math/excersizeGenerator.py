269c4698ea5b83d408ec1918b59c7d225d2824b1
semerded <sem.van.broekhoven@gmail.com>
2024-03-30 18:23:16 +0100

    added a new (lower) abstraction layer for the excercise generator

--- newcode/core/math/excersizeGenerator.py
+++ newcode/core/math/excersizeGenerator.py
@@ -1,4 +1,5 @@
-import random
+import random, core.math_excercise_generator as ex
+
 
 def _removeDecimal_0_fromNumber(number: float):
     if (roundNumber := int(number)) == number:
@@ -8,64 +9,48 @@ def _removeDecimal_0_fromNumber(number: float):
 def _generateDecimalNumber(maxNumber: int, floatingPointAmount: int = 0):
     return _removeDecimal_0_fromNumber(round(random.random() * (maxNumber - 1) + 1, floatingPointAmount))
 
-def calculate(number1, number2, operand):
-    if operand == "+":
-        return number1 + number2
-    elif operand == "-":
-        return number1 - number2
-    elif operand in ("*", "maaltafel"):
-        return number1 * number2
-    elif operand == "/":
-        return number1 / number2
-    elif "%" in operand:
-        return number1 / 100 * number2
-
-def _checkSolutionFloatingPointAmount(number1: float, number2: float, operand: str, floatingPointAmout: int = 0):
+def _checkIfSolutionHasSameFloatingPointAmount(number1: float, number2: float, operand: str, floatingPointAmout: int = 0):
     solution = calculate(number1, number2, operand)
     return round(solution, floatingPointAmout) == solution
         
-        
+def calculate(number1, number2, operand):
+    return ex.calculate(number1, number2, operand) # overwrite base function
 
 
+"""
+implementations of the 'math_excercise_generator' functions
+"""
 def addition(maxNumber: int, maxSolution: int, floatingPointAmount: int = 0):
     while True:
-        number1 = _generateDecimalNumber(maxNumber, floatingPointAmount)
-        number2 = _generateDecimalNumber(maxNumber, floatingPointAmount)
-        if number1 + number2 <= maxSolution and number1 + number2 >= 0 and _checkSolutionFloatingPointAmount(number1, number2, "+", floatingPointAmount):
+        number1, number2 = ex.addition(maxNumber, maxSolution, floatingPointAmount)
+        if _checkIfSolutionHasSameFloatingPointAmount(number1, number2, "+", floatingPointAmount):
             return number1, number2
         
 def subtraction(maxNumber: int, maxSolution: int, floatingPointAmount: int = 0):
     while True:
-        number1 = _generateDecimalNumber(maxNumber, floatingPointAmount)
-        number2 = _generateDecimalNumber(maxNumber, floatingPointAmount)
-        if number1 - number2 <= maxSolution and number1 - number2 >= 0 and _checkSolutionFloatingPointAmount(number1, number2, "-", floatingPointAmount):
+        number1, number2 = ex.subtraction(maxNumber, maxSolution, floatingPointAmount)
+        if _checkIfSolutionHasSameFloatingPointAmount(number1, number2, "-", floatingPointAmount):
             return number1, number2
 
 def multiplication(maxNumber: int, maxSolution: int, floatingPointAmount: int = 0, secondNumberDecimal = False):
     while True:
-        secondNumberFloatingPointAmout = floatingPointAmount if secondNumberDecimal else 0
-        number1 = _generateDecimalNumber(maxNumber / (maxSolution / 100), floatingPointAmount)
-        number2 = _generateDecimalNumber(maxNumber / (maxSolution / 100), secondNumberFloatingPointAmout)
-        if number1 * number2 <= maxSolution and number1 * number2 >= 0 and _checkSolutionFloatingPointAmount(number1, number2, "*", floatingPointAmount):
+        number1, number2 = ex.multiplication(maxNumber / (maxSolution / 100), floatingPointAmount, secondNumberDecimal)
+        if _checkIfSolutionHasSameFloatingPointAmount(number1, number2, "*", floatingPointAmount):
             return number1, number2
         
 def division(maxNumber: int, maxSolution: int, floatingPointAmount: int = 0, secondNumberDecimal = False):
     while True:
-        secondNumberFloatingPointAmout = floatingPointAmount if secondNumberDecimal else 0
-        number1 = _generateDecimalNumber(maxNumber / (maxSolution / 100), floatingPointAmount)
-        number2 = _generateDecimalNumber(maxNumber / (maxSolution / 100), secondNumberFloatingPointAmout)
-        try:
-            if number1 / number2 <= maxSolution and number1 / number2 >= 0 and _checkSolutionFloatingPointAmount(number1, number2, "/", floatingPointAmount) and number1 != number2:
-                return number1, number2
-        except ZeroDivisionError: ... # rule out a crash from a division by zero
+        number1, number2 = ex.division(maxNumber / (maxSolution / 100), maxSolution, floatingPointAmount, secondNumberDecimal)
+        if _checkIfSolutionHasSameFloatingPointAmount(number1, number2, "/", floatingPointAmount) and number1 != number2:
+            return number1, number2
         
 def multiplicationTables(*void): #? outrule crash when arguments are givin
     return multiplication(10, 100)
 
 def percentage(maxNumber: int, void: None, floatingPointAmount: int = 0):
     while True:
-        _percentage = random.randint(1, 100)
-        number = _generateDecimalNumber(int(maxNumber / 100), floatingPointAmount)
+        _percentage, number = ex.percentage(maxNumber / 100, floatingPointAmount)
+        number = int(number)
         if _percentage / 100 * number == round(_percentage / 100 * number, floatingPointAmount):  # TODO
             return _percentage, number
         
