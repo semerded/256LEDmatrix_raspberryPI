0f015895b79cd8205c0e3839daf18af79a930ca8
Sem Van Broekhoven <sem.van.broekhoven@gmail.com>
2024-04-11 22:22:22 +0200

    fixed typing error

--- newcode/LEDs/rear_light_segment.py
+++ newcode/LEDs/rear_light_segment.py
@@ -7,7 +7,7 @@ def main():
     frontLight = _Segment(board.D12, ledCount, 0.4)
 
     while True:
-        data = frontLight.loadData("front_light")
+        data = frontLight.loadData("rear_light")
         frontLight.pixels.fill(data["color"])
         
         sleep(0.1)
