50e09c22a7772489b18e0acac93c3cc0d2e57a35
Sem Van Broekhoven <sem.van.broekhoven@gmail.com>
2024-04-11 21:33:14 +0200

    added front and rear lights code to call.py

--- newcode/call.py
+++ newcode/call.py
@@ -15,5 +15,14 @@ def main():
 def knightRider():
     subprocess.call("sudo python3 LED_knightRider.py", shell=True)
     
+def frontLight():
+    subprocess.call("sudo python3 LED_front.py", shell=True)
+    
+def rearLight():
+    subprocess.call("sudo python3 LED_rear.py", shell=True)
+        
 threading.Thread(target=main).start()
 threading.Thread(target=knightRider).start()
+threading.Thread(target=frontLight).start()
+threading.Thread(target=rearLight).start()
+print("calling done")
