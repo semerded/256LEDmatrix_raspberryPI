3630bade0a75e78013c54c1f7ed2f856205da6da
Sem Van Broekhoven <sem.van.broekhoven@gmail.com>
2024-04-15 21:55:46 +0200

    testing code for car light

--- newcode/LEDs/car_light_segment.py
+++ newcode/LEDs/car_light_segment.py
@@ -2,12 +2,14 @@ def main():
     from LEDs._segment import _Segment
     from time import sleep
     import board
+    
+    from LEDs.LEDeffects import rider, static, rainbowfull, rainbowRider
 
-    ledCount: int = 80 # 80
-    frontLight = _Segment(board.D10, ledCount, 0.1)
+    ledCount: int = 112 # 80, 32 rear
+    carlight = _Segment(board.D10, ledCount, 0.1)
 
     while True:
-        data = frontLight.loadData("car_light")
-        frontLight.pixels.fill(data["color"])
+        data = carlight.loadData("car_light")
+        rider(carlight, data["color"], ledCount)
         
         sleep(0.1)
