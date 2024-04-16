bd4a7244e85c49a14fadbc24af5abf0c80cb365c
Sem Van Broekhoven <sem.van.broekhoven@gmail.com>
2024-04-16 21:41:12 +0200

    added a effect getter by string (name)

--- newcode/LEDs/LEDeffects.py
+++ newcode/LEDs/LEDeffects.py
@@ -1,46 +1,47 @@
 from time import sleep
 from gFrame.elements.colors import Color
 
-currentLED = 0
-currentRainbow = 0
-ascending = True
+class LEDeffects:
+    rainbowColors = [Color.RED, Color.ORANGE, Color.YELLOW, Color.GREEN, Color.BLUE, Color.PINK, Color.PURPLE]
+    _currentLED = 0
+    _currentRainbow = 0
+    _ascending = True
+    
+    def __init__(self, pixels, ledAmount: int) -> None:
+        self.pixels = pixels
+        self.ledAmount = ledAmount
+        self._effectNames = {"static": self.static, "rainbow": self.rainbowfull, "rider": self.rider, "rainbowrider": self.rainbowRider}
 
-rainbowColors = [Color.RED, Color.ORANGE, Color.YELLOW, Color.GREEN, Color.BLUE, Color.PINK, Color.PURPLE]
-def rainbowfull(pixels):
-    global currentRainbow
-    static(pixels, rainbowColors[currentRainbow])
-    currentRainbow += 1
-    if currentRainbow == len(rainbowColors):
-        currentRainbow = 0
+    def rainbowfull(self, *void):
+        self.static(self.rainbowColors[self._currentRainbow])
+        self._currentRainbow += 1
+        if self._currentRainbow == len(self.rainbowColors):
+            self._currentRainbow = 0
         
-
-def static(pixels, color):
-    pixels.fill(color)
+    def static(self, color):
+        self.pixels.fill(color)
         
+    def rainbowRider(self, *void):
+        self.rider(self.pixels, self.rainbowColors[self._currentRainbow], self.ledAmount)
+        self._currentRainbow += 1
+        if self._currentRainbow == len(self.rainbowColors):
+            self._currentRainbow = 0
+    
+    def rider(self, color):
+        self.pixels.pixels[self._currentLED] = color
         
-def rainbowRider(pixels, ledAmount):
-    global currentRainbow
-    rider(pixels, rainbowColors[currentRainbow], ledAmount)
-    currentRainbow += 1
-    if currentRainbow == len(rainbowColors):
-        currentRainbow = 0
-    
+        if self._ascending and self._currentLED > 0:
+            self.pixels.pixels[self._currentLED - 1] = (0, 0, 0)
+        elif not self._ascending and self._currentLED < self.ledAmount - 1:
+            self.pixels.pixels[self._currentLED + 1] = (0, 0, 0)
+        
+        if self._currentLED == 0 and not self._ascending:
+            self._ascending = True
+        elif self._currentLED == self.ledAmount - 1 and self._ascending:
+            self._ascending = False 
 
-def rider(pixels, color, ledAmount):
-    global currentLED, ascending
-    pixels.pixels[currentLED] = color
-    
-    if ascending and currentLED > 0:
-        pixels.pixels[currentLED - 1] = (0, 0, 0)
-    elif not ascending and currentLED < ledAmount - 1:
-        pixels.pixels[currentLED + 1] = (0, 0, 0)
-    
-    if currentLED == 0 and not ascending:
-        ascending = True
-    elif currentLED == ledAmount - 1 and ascending:
-        ascending = False 
-
-    currentLED += 1 if ascending else - 1
-    
-    
+        self._currentLED += 1 if self._ascending else - 1
+        
+    def getEffectByName(self, name: str):
+        return self._effectNames[name]
     
--- newcode/LEDs/car_light_segment.py
+++ newcode/LEDs/car_light_segment.py
@@ -3,13 +3,14 @@ def main():
     from time import sleep
     import board
     
-    from LEDs.LEDeffects import rider, static, rainbowfull, rainbowRider
-
     ledCount: int = 16 # 80, 32 rear (112)
     carlight = _Segment(board.D10, ledCount, 0.1)
+    
+    from LEDs.LEDeffects import LEDeffects
+    
+    ledEffects = LEDeffects(carlight, ledCount)
 
     while True:
         data = carlight.loadData("car_light")
-        rainbowRider(carlight, ledCount)
-        
+        ledEffects.getEffectByName(data["type"])
         sleep(0.1)
