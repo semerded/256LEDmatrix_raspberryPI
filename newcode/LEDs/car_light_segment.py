caad0328659c41fb3cad10c6b210d42772226afa
semerded <sem.van.broekhoven@gmail.com>
2024-05-08 14:14:16 +0200

    only effects with speed controll get the sleep in the car light segment

--- newcode/LEDs/car_light_segment.py
+++ newcode/LEDs/car_light_segment.py
@@ -2,8 +2,9 @@ def main():
     from LEDs._segment import _Segment
     from time import sleep
     import board
+    from LEDs.LEDeffects import ledEffectsProperties
     
-    ledCount: int = 16 # 80, 32 rear (112)
+    ledCount: int = 136 # 80, 56 rear (112)
     carlight = _Segment(board.D10, ledCount, 0.1)
     
     from LEDs.LEDeffects import LEDeffects
@@ -13,4 +14,5 @@ def main():
     while True:
         data = carlight.loadData("car_light")
         ledEffects.getEffectByName(data["type"], data["color"])
-        sleep(data["speed"])
+        if ledEffectsProperties[data["type"]]["speed"]:
+            sleep(data["speed"])
