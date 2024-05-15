dc104f9ffb9e2b80d6da1775f2fad48af38e9f2d
semerded <sem.van.broekhoven@gmail.com>
2024-05-15 12:14:25 +0200

    changed rider effect

--- newcode/LEDs/LEDeffects.py
+++ newcode/LEDs/LEDeffects.py
@@ -30,6 +30,7 @@ class LEDeffects:
     _currentLED = 0
     _currentRainbow = 0
     _ascending = True
+    _activeLEDperRing = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
     
     def __init__(self, pixels, ledAmount: int) -> None:
         self.pixels = pixels
@@ -53,49 +54,61 @@ class LEDeffects:
             self._currentRainbow = 0
     
     def rider(self, color):
-        self.pixels.pixels[self._currentLED] = color
-        
-        if self._ascending and self._currentLED > 0:
-            self.pixels.pixels[self._currentLED - 1] = (0, 0, 0)
-        elif not self._ascending and self._currentLED < self.ledAmount - 1:
-            self.pixels.pixels[self._currentLED + 1] = (0, 0, 0)
-        
-        if self._currentLED == 0 and not self._ascending:
-            self._ascending = True
-        elif self._currentLED == self.ledAmount - 1 and self._ascending:
-            self._ascending = False 
-
-        self._currentLED += 1 if self._ascending else - 1
+        self._riderPerRing(carLedMapping["voor"]["links"][0], 0, color)
+        self._riderPerRing(carLedMapping["voor"]["links"][1], 1, color)
+        self._riderPerRing(carLedMapping["voor"]["links"][2], 2, color)
+        self._riderPerRing(carLedMapping["voor"]["rechts"][0], 3, color)
+        self._riderPerRing(carLedMapping["voor"]["rechts"][1], 4, color)
+        self._riderPerRing(carLedMapping["voor"]["rechts"][2], 5, color)
+        self._riderPerRing(carLedMapping["achter"]["links"][0], 6, color)
+        self._riderPerRing(carLedMapping["achter"]["links"][1], 7, color)
+        self._riderPerRing(carLedMapping["achter"]["rechts"][0], 8, color)
+        self._riderPerRing(carLedMapping["achter"]["rechts"][1], 9, color)
+            
 
     def car(self, *void):
-        self._colorLedRing(carLedMapping["voor"]["links"][0], Color.ORANGE)
-        self._colorLedRing(carLedMapping["voor"]["rechts"][0], Color.ORANGE)
-        self._colorLedRing(carLedMapping["achter"]["links"][0], Color.ORANGE)
-        self._colorLedRing(carLedMapping["achter"]["rechts"][0], Color.ORANGE)
+        self._colorLedRing(carLedMapping["voor"]["links"][1], Color.ORANGE)
+        self._colorLedRing(carLedMapping["voor"]["rechts"][1], Color.ORANGE)
+        self._colorLedRing(carLedMapping["achter"]["links"][1], Color.ORANGE)
+        self._colorLedRing(carLedMapping["achter"]["rechts"][1], Color.ORANGE)
         
-        self._colorLedRing(carLedMapping["voor"]["links"][1], Color.WHITE)
+        self._colorLedRing(carLedMapping["voor"]["links"][0], Color.WHITE)
         self._colorLedRing(carLedMapping["voor"]["links"][2], Color.WHITE)
         
-        self._colorLedRing(carLedMapping["voor"]["rechts"][1], Color.WHITE)
+        self._colorLedRing(carLedMapping["voor"]["rechts"][0], Color.WHITE)
         self._colorLedRing(carLedMapping["voor"]["rechts"][2], Color.WHITE)
         
-        self._colorLedRing(carLedMapping["achter"]["links"][1], Color.WHITE)
-        self._colorLedRing(carLedMapping["achter"]["rechts"][1], Color.WHITE)
+        self._colorLedRing(carLedMapping["achter"]["links"][0], Color.RED)
+        self._colorLedRing(carLedMapping["achter"]["rechts"][0], Color.RED)
         
-        sleep(0.5)
+        sleep(0.3)
         
-        self._colorLedRing(carLedMapping["voor"]["links"][0], Color.BLACK)
-        self._colorLedRing(carLedMapping["voor"]["rechts"][0], Color.BLACK)
-        self._colorLedRing(carLedMapping["achter"]["links"][0], Color.BLACK)
-        self._colorLedRing(carLedMapping["achter"]["rechts"][0], Color.BLACK)
+        self._colorLedRing(carLedMapping["voor"]["links"][1], Color.BLACK)
+        self._colorLedRing(carLedMapping["voor"]["rechts"][1], Color.BLACK)
+        self._colorLedRing(carLedMapping["achter"]["links"][1], Color.BLACK)
+        self._colorLedRing(carLedMapping["achter"]["rechts"][1], Color.BLACK)
         
-        sleep(0.5)
+        sleep(0.3)
 
         
     def getEffectByName(self, name: str, color):
         self._effectNames[name](color)
         
     def _colorLedRing(self, ledRange: tuple[int, int], color):
-            for index in range(ledRange[0], ledRange[1] + 1):
-                self.pixels.pixels[index] = color
+        for index in range(ledRange[0], ledRange[1] + 1):
+            self.pixels.pixels[index] = color
+                
+    def _riderPerRing(self, ledRange: tuple[int, int], index, color):
+        _index = self._activeLEDperRing[index] + 1
+        if _index + ledRange[0] > ledRange[1]:
+            _index = 0
+        
+        if _index == 0:
+            self.pixels.pixels[ledRange[1]] = Color.BLACK
+        else:    
+            self.pixels.pixels[ledRange[0] + _index - 1] = Color.BLACK
+        self.pixels.pixels[_index] = color
+        
+        
+            
     
